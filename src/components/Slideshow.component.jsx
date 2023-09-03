3850f3be9557a955bc9e2bbf44bb7a92c3798503
Tristan Roulet <127687937+TristanRoulet@users.noreply.github.com>
2023-09-04 00:49:41 +0200

    Deuxième correction du composant Slideshow

--- src/components/Slideshow.component.jsx
+++ src/components/Slideshow.component.jsx
@@ -1,41 +1,44 @@
 import React, { useState } from 'react';
 import arrow from '../assets/images/slideshow_arrow.png'
 
-const Carousel = ({ images }) => {
+export default function Carousel(props) {
+    const { images } = props
     const [currentSlide, setCurrentSlide] = useState(0);
 
     const prevSlide = () => {
-      setCurrentSlide((prevSlide) =>
-        prevSlide === 0 ? images.length - 1 : prevSlide - 1
-      );
-    };
+        setCurrentSlide((prevSlide) =>
+            prevSlide === 0 ? images.length - 1 : prevSlide - 1
+        )
+    }
     
     const nextSlide = () => {
-      setCurrentSlide((prevSlide) =>
-        prevSlide === images.length - 1 ? 0 : prevSlide + 1
-      );
-    };
+        setCurrentSlide((prevSlide) =>
+            prevSlide === images.length - 1 ? 0 : prevSlide + 1
+        )
+    }
    
     return (
-      <div className='slideshow'>
-        <div className='slide'>
-            <img src={images[currentSlide]} alt={`${currentSlide}`} className='slide-picture' />
+        <div className='slideshow'>
+            { images && (
+                <>
+                    <div className='slide'>
+                        <img src={images[currentSlide]} alt={`${currentSlide}`} className='slide-picture' />
+                    </div>
+                    {images.length > 1 && (
+                        <>
+                            <button className='slideshow-btn left' onClick={prevSlide}>
+                                <img src={arrow} alt='previous' className='arrow' />
+                            </button>
+                            <button className='slideshow-btn right' onClick={nextSlide}>
+                                <img src={arrow} alt='next' className='arrow' />
+                            </button>
+                            <div className='slide-number'>
+                                {currentSlide + 1}/{images.length}
+                            </div>
+                        </>
+                    )}
+                </>
+            )}
         </div>
-        {images.length > 1 && (
-          <>
-            <button className='slideshow-btn left' onClick={prevSlide}>
-                <img src={arrow} alt='previous' className='arrow' />
-            </button>
-            <button className='slideshow-btn right' onClick={nextSlide}>
-                <img src={arrow} alt='next' className='arrow' />
-            </button>
-            <div className='slide-number'>
-                {currentSlide + 1}/{images.length}
-            </div>
-          </>  
-        )}
-      </div>
-    );
-  };
-
-export default Carousel;
+    )
+}
