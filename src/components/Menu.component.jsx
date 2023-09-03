2364d12247411f9eba05b8a1cab8c5eb76ce57e3
Tristan Roulet <127687937+TristanRoulet@users.noreply.github.com>
2023-09-04 00:47:11 +0200

    Correction composant Menu

--- src/components/Menu.component.jsx
+++ src/components/Menu.component.jsx
@@ -1,22 +1,20 @@
-import React, { useState } from 'react'
+import React from 'react'
 import arrow from '../assets/images/menu_arrow.png'
 
 export default function Menu(props) {
-    const [isOpen, setIsOpen] = useState(false)
-    
-    const { title, description } = props
+    const { title, description, isOpen, onClick } = props
 
     return (
-        <article className='menu'>
-            <div className='menu-header'>
-                <h3 className='menu-title'>{title || "Titre non défini"}</h3>
-                <button className={`menu-button ${isOpen && "active"}`} onClick={() => setIsOpen(!isOpen)}>
-                    <img src={arrow} alt='Menu Arrow' className='menu-arrow'></img>
-                </button>
-            </div>
-            <div className={`menu-description ${isOpen && "active"}`}>
-                <p className='menu-text'>{description ||"Description non définie"}</p>
-            </div>
-        </article>
+      <article className={`menu ${isOpen && 'active'}`}>
+        <div className='menu-header'>
+          <h3 className='menu-title'>{title || 'Titre non défini'}</h3>
+          <button className={`menu-button ${isOpen && 'active'}`} onClick={onClick}>
+            <img src={arrow} alt='Menu Arrow' className='menu-arrow'></img>
+          </button>
+        </div>
+        <div className={`menu-description ${isOpen && 'active'}`}>
+          <p className='menu-text'>{description || 'Description non définie'}</p>
+        </div>
+      </article>
     )
 }
